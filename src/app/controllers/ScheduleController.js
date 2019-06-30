f32cadada95d19f5188b19923bffad87ce2dabd6
Sidney Santos <sidney_ssantos@hotmail.com>
2019-06-29 22:02:22 -0300

    Modulo 03 - Listando agenda do prestador

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -11,7 +11,7 @@ class ScheduleController {
     });
 
     if (!checkUserProvider) {
-      return res.status(401).json({ error: 'User is no a provider' });
+      return res.status(401).json({ error: 'User is not a provider' });
     }
 
     const { date } = req.query;
