66790473212a663c0a55e3a12158180732930bed
Sidney Santos <sidney_ssantos@hotmail.com>
2019-06-30 20:40:17 -0300

    Modulo 03 - Configurando fila com Reds

---
+++ src/lib/Queue.js
@@ -0,0 +1,41 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+
+    // criação de fila
+    this.init();
+  }
+
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        // conexão com o redis
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        // método handle vai processar o job (fila)
+        handle,
+      };
+    });
+  }
+
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
+}
+
+export default new Queue();
---
+++ src/queue.js
@@ -0,0 +1,5 @@
+import Queue from './lib/Queue';
+
+// foi feito em arquivo separado porque não irá rodar no mesmo node
+// podemos ter a aplicação rodando em um terminal e a fila em outro
+Queue.processQueue();
